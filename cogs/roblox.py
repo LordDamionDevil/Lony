f1e609739c5706d4ee25210f683d64111a6cf6e8
Maurice Piontek <robloxfilmcam@gmail.com>
2018-06-02 18:41:55 +0200

    Update roblox.py

--- cogs/roblox.py
+++ cogs/roblox.py
@@ -11,6 +11,7 @@ import aiohttp
 import asyncio
 import codecs
 import json
+import re
 from threading import Thread
 from bs4 import BeautifulSoup
 import requests
@@ -23,9 +24,9 @@ class roblox:
         self.s = requests.session()
         self.token = None
 
-    """@commands.command()
+    @commands.command()
     async def rbxrap(self, username : str, encoding='utf-8', errors='strict'):
-         Gets the Rap from A ROBLOX User
+         """Gets the Rap from A ROBLOX User"""
          user = str.format(username)
          url = "https://api.roblox.com/users/get-by-username?username=" + user
          await self.bot.say("Searching ROBLOX For " + user + "'s ID")
@@ -39,7 +40,7 @@ class roblox:
                  data = await r2.read()
                  result2 = self.decoder.decode(data)
                  user = str.format(username)
-                 await self.bot.say(user + "'s Rap Is " + format(result2))"""
+                 await self.bot.say(user + "'s Rap Is " + format(result2))
                  
     @commands.command()
     async def rbxuserinfo(self, username : str):
@@ -48,9 +49,6 @@ class roblox:
         url = "https://www.roblox.com/Groups/GetPrimaryGroupInfo.ashx?users=" + user
         async with aiohttp.get(url) as r:
             result = await r.json()
-            url3 = "https://api.roblox.com/groups/" + result['HQ_Trivia']['GroupId']
-            async with aiohttp.get(url3) as r3:
-            result3 = await r3.json()
             url2 = "https://api.roblox.com/users/get-by-username?username=" + user
             async with aiohttp.get(url2) as r2:
                 result2 = await r2.json()
@@ -60,19 +58,34 @@ class roblox:
                     do = await av.json()
                     if do['Final'] == True:
                         pic = format(do['Url'])
-                        pic2 = format(result3['EmblemUrl'])
                         data = discord.Embed(description="Username: " + user)
                         data.add_field(name="Items", value="Soon")
                         data.add_field(name="Info", value="ROBLOX is a Free MMO Which allows you to be creative and play other players games!")
                         data.set_thumbnail(url=pic)
                         data.set_footer(text="Info of the user: " + user + "! Thanks to ROBLOX for their APIs!")
-                        data2 = discord.Embed(description="Primary Group from: " + user)
-                        data2.add_field(name="Description", value=result3['Description'])
-                        data2.set_thumbnail(url=pic2)
-                        data2.set_footer(text="Info of group: " + result3['Name'] + "! Thanks to ROBLOX for their APIs!")
-                        await self.bot.say(embed=data) self.bot.say(embed=data2)
+                        await self.bot.say(embed=data)
                     if do['Final'] == False:
-                        await self.bot.say("Avatar Pic Of " + user + " Was not rendered yet! Please Try Again!")
+                        pic = format("https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF")
+                        data = discord.Embed(description="Username: " + user)
+                        data.add_field(name="Items", value="Soon")
+                        data.add_field(name="Info", value="ROBLOX is a Free MMO Which allows you to be creative and play other players games!")
+                        data.set_thumbnail(url=pic)
+                        data.set_footer(text="Info of the user: " + user + "! Thanks to ROBLOX for their APIs!")
+                        await self.bot.say(embed=data)
+                    """self.bot.say(Avatar Pic Of  + user +  Was not rendered yet! Please Try Again!)"""
+                    gid = format(int(result[user]['GroupId']))
+                    da = "https://api.roblox.com/groups/" + gid
+                    async with aiohttp.get(da) as a:
+                        ass = await a.json()
+                        im = ass['EmblemUrl']
+                        inum = re.sub('[^0-9]', '', im)
+                        pic2 = format("https://www.roblox.com/Thumbs/Asset.ashx?width=140&height=140&assetId=" + inum)
+                        data2 = discord.Embed(description="Primary Group from: " + user)
+                        data2.add_field(name="Description", value=ass['Description'])
+                        data2.set_thumbnail(url=pic2)
+                        data2.set_footer(text="Info of group: " + ass['Name'] + "! Thanks to ROBLOX for their APIs!")
+                        await self.bot.say(embed=data2)
+                        
     
 def setup(bot):
     n = roblox(bot)
